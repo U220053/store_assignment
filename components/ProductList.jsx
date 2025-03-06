a9b015afc8d63f00915a89d5dde25918e9a0acb2
Nitu Kumari <nitukumari@Nitus-MacBook-Air.local>
2025-03-06 17:23:44 +0530

    images added

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,4 +1,5 @@
 // components/ProductList.jsx
+import Image from "next/image";
 export default function ProductList({ products, addToCart }) {
   return (
     <div className="product-list">
@@ -9,6 +10,18 @@ export default function ProductList({ products, addToCart }) {
           {products.map((product) => (
             <div key={product.id} className="product-card">
               <h3 className="product-title">{product.name}</h3>
+              {/* <Image
+                className="product-image"
+                src={product.image}
+                alt={`Product image of ${product.name}`}
+                width={200}
+                height={200}
+              /> */}
+              <img
+                src={product.image}
+                alt={product.name}
+                style={{ width: "100%", height: "200px", objectFit: "cover" }}
+              />
               <p className="product-description">{product.description}</p>
               <p className="product-price">${product.price.toFixed(2)}</p>
               <button
